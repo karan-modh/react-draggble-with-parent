a1874088271206dde63f1b994e21487fd0738eb3
Karan Modh <karanmodh71@gmail.com>
2021-07-08 23:08:14 +0530

    Made the position of new parent frame set to child's location

--- src/App.js
+++ src/App.js
@@ -5,25 +5,51 @@ import './App.css';
 class App extends React.Component {
 	constructor(props) {
 		super(props);
+
 		this.state = {
-			containers: [<Frame key={1} name="1" width={(1)*150}/>],
+			containers: {
+				key: 1,
+				name: "1",
+				height: (1)*50,
+				width: (1)*150,
+				x: 0,
+				y: 0,
+				child: {}
+			},
 			numContainers: 1,
 		}
 
 		this.addParent = this.addParent.bind(this);
 	}
 
+	setFramePos(data) {
+		this.setState({
+			containers: Object.assign({}, this.state.containers, {
+				x: data.x,
+				y: data.y
+			})
+		});
+		console.log(this.state.containers)
+	}
+
 	addParent(event) {
 		const containers = this.state.containers;
 		const numContainers = this.state.numContainers;
-		console.log(numContainers);
 
 		this.setState({
-			containers: [<Frame key={numContainers+1} name={(numContainers+1).toString()} Children={containers[0]} width={(numContainers+1)*150}/>],
+			containers: {
+				key: numContainers+1,
+				name: (numContainers+1).toString(),
+				height: (numContainers+1)*50,
+				width: (numContainers+1)*150,
+				x: containers.x,
+				y: containers.y,
+				child: containers
+			},
 			numContainers: numContainers + 1
 		});
 	}
- 
+
 	render() {
 		return (
 			<div>
@@ -31,7 +57,7 @@ class App extends React.Component {
 					<button className="btn btn-primary large" onClick={this.addParent}> Add Parent</button>
 				</div>
 				<div className="row content_body">
-					{this.state.containers}
+					<Frame containers={this.state.containers}  setFramePos={this.setFramePos}/>
 				</div>
 			</div>
 		);
--- src/Frame.js
+++ src/Frame.js
@@ -4,28 +4,32 @@ import Draggable from 'react-draggable';
 
 class Frame extends react.Component {
 
+    handleStop = (event,data) => {
+        console.log(data);
+    };
+
     render() {
+        console.log(this.props);
+        var child_frame;
+        if (this.props.containers.child && Object.keys(this.props.containers.child).length !== 0 ) {
+            child_frame = <Frame containers={this.props.containers.child} setFramePos={this.props.setFramePos}/>
+        }
+        else {
+            child_frame = <div></div>
+        }
         return(
             <Draggable
                 axis="both"
                 bounds="parent"
-                // offsetParent={document.querySelector('.content_body')}
-                handle={".frame_"+this.props.name}
-                defaultPosition={{x: 0, y: 0}}
-                position={null}
+                handle={".frame_"+this.props.containers.name}
+                defaultPosition={{x:this.props.containers.x, y:this.props.containers.y}}
+                // position={null}
                 grid={[25, 25]}
                 scale={1}
-                onStart={this.handleStart}
-                onDrag={this.handleDrag}
                 onStop={this.handleStop}>
-                <div className="frame_content" style={{width: this.props.width}}>
-                    <div className={"frame_title frame_"+this.props.name}>Click {this.props.name}</div>
-                    {/* <div className="child_frames"> */}
-                        &nbsp; &nbsp;
-                        <br/>
-                        &nbsp; &nbsp;
-                        {this.props.Children}
-                    {/* </div> */}
+                <div className="frame_content" style={{width: this.props.containers.width, height: this.props.containers.height}}>
+                    <div className={"frame_title frame_"+this.props.containers.name} >Click {this.props.containers.name}</div>
+                    {child_frame}
                 </div>
             </Draggable>
         );

20e751ba81c67c62aedb3224f5d14d9b8b18bdb0
Karan Modh <karanmodh71@gmail.com>
2021-07-10 17:50:09 +0530

    Revert back to #a187408

--- src/App.js
+++ src/App.js
@@ -7,55 +7,47 @@ class App extends React.Component {
 		super(props);
 
 		this.state = {
-			containers: [{
+			containers: {
 				key: 1,
 				name: "1",
 				height: (1)*50,
 				width: (1)*150,
 				x: 0,
 				y: 0,
-			}],
+				child: {}
+			},
 			numContainers: 1,
 		}
 
 		this.addParent = this.addParent.bind(this);
-		this.setFramePos = this.setFramePos.bind(this);
 	}
 
-	setFramePos(delx, dely, name) {
-		const key = parseInt(name);
-		var containers = [...this.state.containers];
-		var frame = containers[key-1];
-		frame = {
-			...containers[key-1],
-			x: delx,
-			y: dely
-		}
-		containers[key-1] = frame
+	setFramePos(data) {
 		this.setState({
-			containers: containers
-		})
+			containers: Object.assign({}, this.state.containers, {
+				x: data.x,
+				y: data.y
+			})
+		});
+		console.log(this.state.containers)
 	}
 
 	addParent(event) {
 		const containers = this.state.containers;
-		// const x = this.state.containers.x;
-		// const y = this.state.containers.y;
 		const numContainers = this.state.numContainers;
-		const last_frame = containers[containers.length-1]
+
 		this.setState({
-			containers: containers.concat([{
-				key: last_frame.key + 1,
-				name: (last_frame.key + 1).toString(),
-				height: (last_frame.key + 1)*50,
-				width: (last_frame.key + 1)*150,
-				x: last_frame.x - 10,
-				y: last_frame.y - 10
-			}]),
+			containers: {
+				key: numContainers+1,
+				name: (numContainers+1).toString(),
+				height: (numContainers+1)*50,
+				width: (numContainers+1)*150,
+				x: containers.x,
+				y: containers.y,
+				child: containers
+			},
 			numContainers: numContainers + 1
 		});
-
-		// console.log(this.state.containers)
 	}
 
 	render() {
@@ -65,7 +57,7 @@ class App extends React.Component {
 					<button className="btn btn-primary large" onClick={this.addParent}> Add Parent</button>
 				</div>
 				<div className="row content_body">
-					<Frame containers={this.state.containers} idx={1} setFramePos={this.setFramePos}/>
+					<Frame containers={this.state.containers}  setFramePos={this.setFramePos}/>
 				</div>
 			</div>
 		);
--- src/Frame.js
+++ src/Frame.js
@@ -5,18 +5,14 @@ import Draggable from 'react-draggable';
 class Frame extends react.Component {
 
     handleStop = (event,data) => {
-        console.log(data)
-        var idx = this.props.containers.length - this.props.idx;
-        this.props.setFramePos(data.x, data.y, this.props.containers[idx].name);
+        console.log(data);
     };
 
     render() {
-        // console.log(this.props);
-        const idx = this.props.containers.length - this.props.idx;
-        // console.log(idx);
+        console.log(this.props);
         var child_frame;
-        if (idx > 0) {
-            child_frame = <Frame containers={this.props.containers} idx={this.props.idx+1} setFramePos={this.props.setFramePos} />
+        if (this.props.containers.child && Object.keys(this.props.containers.child).length !== 0 ) {
+            child_frame = <Frame containers={this.props.containers.child} setFramePos={this.props.setFramePos}/>
         }
         else {
             child_frame = <div></div>
@@ -25,14 +21,14 @@ class Frame extends react.Component {
             <Draggable
                 axis="both"
                 bounds="parent"
-                handle={".frame_"+this.props.containers[idx].name}
-                defaultPosition={{x:this.props.containers[idx].x, y:this.props.containers[idx].y}}
+                handle={".frame_"+this.props.containers.name}
+                defaultPosition={{x:this.props.containers.x, y:this.props.containers.y}}
                 // position={null}
                 grid={[25, 25]}
                 scale={1}
                 onStop={this.handleStop}>
-                <div className="frame_content" style={{width: this.props.containers[idx].width, height: this.props.containers[idx].height}}>
-                    <div className={"frame_title frame_"+this.props.containers[idx].name} >Click {this.props.containers[idx].name}</div>
+                <div className="frame_content" style={{width: this.props.containers.width, height: this.props.containers.height}}>
+                    <div className={"frame_title frame_"+this.props.containers.name} >Click {this.props.containers.name}</div>
                     {child_frame}
                 </div>
             </Draggable>

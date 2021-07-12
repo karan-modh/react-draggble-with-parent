b7480ec6f45734d8cc316ae46dc7f3d3b0ca011d
Karan Modh <karanmodh71@gmail.com>
2021-07-12 16:48:03 +0530

    added array based containers

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Frame from './Frame';
 import './App.css';
 
+/*
 class App extends React.Component {
 	constructor(props) {
 		super(props);
@@ -65,3 +66,123 @@ class App extends React.Component {
 }
 
 export {App};
+
+*/
+
+// Array format
+
+// /*
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			containers: [{
+				key: 1,
+				name: "1",
+				height: (1)*50,
+				width: (1)*150,
+				x: 0,
+				y: 0,
+			}],
+			numContainers: 1,
+		}
+
+		this.addParent = this.addParent.bind(this);
+		this.setFramePos = this.setFramePos.bind(this);
+		this.printContainers = this.printContainers.bind(this);
+	}
+
+	printContainers() {
+		console.log(this.state.containers)
+	}
+	
+	setFramePos(data, key) {
+		// const key = parseInt(name);
+		var containers = [...this.state.containers];
+		var frame = containers[key-1];
+		frame = {
+			...containers[key-1],
+			x: data.x,
+			y: data.y
+		}
+		containers[key-1] = frame
+		this.setState({
+			containers: containers
+		})
+		// console.log(this.state.containers)
+	}
+	
+	addParent(event) {
+		var containers = this.state.containers;
+		var last_frame = containers[this.state.numContainers - 1];
+
+		// Check for horizontal overflow.
+		var new_x;
+		var last_x;
+		if(last_frame.x + last_frame.width + 150 > window.innerWidth){
+			new_x = last_frame.x - 150;
+			last_x = 150;
+		}
+		else{
+			new_x = last_frame.x;
+			last_x = 0;
+		}
+
+		// Checking vertical overflow.
+		var new_y;
+		var last_y;
+		if(last_frame.y + last_frame.height + 50 > window.innerHeight){
+			new_y = last_frame.y - 50;
+			last_y = 50;
+		}
+		else{
+			new_y = last_frame.y;
+			last_y = 0;
+		}
+
+		console.log(new_x);
+		console.log(new_y);
+		var new_frame = {
+			key: last_frame.key+1,
+			name: (last_frame.key+1).toString(),
+			height: (last_frame.key+1)*50,
+			width: (last_frame.key+1)*150,
+			x: new_x,
+			y: new_y
+		}
+		
+		containers.push(new_frame);
+
+		// console.log(containers);
+		last_frame = {
+			...last_frame,
+			x: last_x,
+			y: last_y
+		}
+		containers[this.state.numContainers - 1] = last_frame;
+		// console.log(containers);
+		this.setState({
+			containers: containers,
+			numContainers: this.state.numContainers + 1
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<div className="row">
+					<button className="btn btn-primary large" onClick={this.addParent}> Add Parent</button>
+					<button className="btn btn-primary large" onClick={this.printContainers}> Print Containers</button>
+				</div>
+				<div className="row content_body">
+					<Frame containers={this.state.containers}  setFramePos={this.setFramePos} idx={this.state.numContainers} />
+				</div>
+			</div>
+		);
+	}
+}
+
+export {App};
+
+// */
--- src/Frame.js
+++ src/Frame.js
@@ -5,14 +5,19 @@ import Draggable from 'react-draggable';
 class Frame extends react.Component {
 
     handleStop = (event,data) => {
-        console.log(data);
+        // console.log(data);
+        // console.log(this.props.idx);
+        this.props.setFramePos(data, this.props.idx);
     };
 
     render() {
         console.log(this.props);
+        var index = this.props.idx-1;
+        const containers = [...this.props.containers];
+        const curr_frame = containers[index];
         var child_frame;
-        if (this.props.containers.child && Object.keys(this.props.containers.child).length !== 0 ) {
-            child_frame = <Frame containers={this.props.containers.child} setFramePos={this.props.setFramePos}/>
+        if (this.props.idx > 1) {
+            child_frame = <Frame containers={containers} setFramePos={this.props.setFramePos} idx={index} />
         }
         else {
             child_frame = <div></div>
@@ -21,14 +26,14 @@ class Frame extends react.Component {
             <Draggable
                 axis="both"
                 bounds="parent"
-                handle={".frame_"+this.props.containers.name}
-                defaultPosition={{x:this.props.containers.x, y:this.props.containers.y}}
+                handle={".frame_" + curr_frame.name}
+                defaultPosition={{x:curr_frame.x, y:curr_frame.y}}
                 // position={null}
-                grid={[25, 25]}
+                grid={[1,1]}
                 scale={1}
                 onStop={this.handleStop}>
-                <div className="frame_content" style={{width: this.props.containers.width, height: this.props.containers.height}}>
-                    <div className={"frame_title frame_"+this.props.containers.name} >Click {this.props.containers.name}</div>
+                <div className="frame_content" style={{width: curr_frame.width, height: curr_frame.height}}>
+                    <div className={"frame_title frame_"+curr_frame.name} >Click {curr_frame.name}</div>
                     {child_frame}
                 </div>
             </Draggable>
